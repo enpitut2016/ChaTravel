7738bafb09eeccd2d58dfb7dcb19470e39b4c246
Katsumi Kumai <kuma.tennis.kttsm@gmail.com>
2016-08-26 00:34:20 +0900

    Create Ueser table

--- ChaTravel/db/schema.rb
+++ ChaTravel/db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825152731) do
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.string   "icon"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
