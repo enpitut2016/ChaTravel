5a9279b9dcbb458d045514a00742e0d36ff12e8a
Katsumi Kumai <kuma.tennis.kttsm@gmail.com>
2016-08-26 15:48:09 +0900

    Make User model (secure password)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   resources :rooms,  :replace_id_with => 'url'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
