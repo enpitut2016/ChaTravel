17040d3056c26b128beb77b242e61644f1f68b50
Katsumi Kumai <kuma.tennis.kttsm@gmail.com>
2016-08-26 17:50:54 +0900

    user new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
+
+  get 'signup'  => 'users#new'
 	root 'static_pages#home'
 	get 'static_pages/help'
 	resources :rooms,  :replace_id_with => 'url'
+	resources :users
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get user_new_url
+    get users_new_url
     assert_response :success
   end
 
