7f6a55fe5e13ba3cf5f768d01095c9b5a5b26308
Satoshi Tanoue <again2222@gmail.com>
2016-08-27 16:55:42 +0900

    localhost/rooms/生成されたroom から localhost/生成されたroomにroutesを変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	root 'static_pages#home'
 	get 'static_pages/help'
 	resources :rooms,  :replace_id_with => 'url'
-
+	get :replace_id_with  => 'rooms'
+	get ':url', to:'rooms#show'
 end
