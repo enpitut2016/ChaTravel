31c7d2f2ae24b56b553be5fff960f7625891eb4b
Satoshi Tanoue <again2222@gmail.com>
2016-08-27 22:01:42 +0900

    newRoomに行くようにルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-	get 'user/new'
 	root 'static_pages#home'
 	get 'static_pages/help'
+	get 'newRoom',to:'static_pages#room_page'
 	resources :rooms,  :replace_id_with => 'url'
 	get :replace_id_with  => 'rooms'
 	get ':url', to:'rooms#show'
+
+	get 'user/new'
 end
