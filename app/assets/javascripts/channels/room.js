7a38cd4828725c8c2c32fe45d5a4bba51e0df337
Takagi Yuki <kiwi.kyuui@gmail.com>
2016-08-28 15:17:17 +0900

    chatメッセージの表示するだけ

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -8,9 +8,15 @@ App.room = App.cable.subscriptions.create({ channel: 'RoomChannel', room: window
   },
 
   received: function(data) {
-    // TODO domに追加
-    console.log(data)
-   
+    var message = "<div class='col-md-9'><p>Name: " + data.user.name + "</p><p>Message: " + data.message+ "</p></div>"
+    var icon = "<div class='col-md-3'><p>icon</p> </div>"
+    var dom = ""
+    if (parseInt(data.user.id) == $('#current_user').data('current_user_id')) {
+      dom =  message + icon
+    } else {
+      dom = icon + message
+    }
+    $('#message_list').append(dom)
   },
 
   speak: function(message) {
