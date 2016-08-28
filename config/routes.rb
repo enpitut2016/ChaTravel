6d7c4b0dffd2acf44d8c02c921c6ef9a75faa56c
Katsumi Kumai <kuma.tennis.kttsm@gmail.com>
2016-08-28 10:13:02 +0900

    make session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'users/new'
 
   get 'signup'  => 'users#new'
-	root 'static_pages#home'
 	get 'static_pages/help'
+
+	get 	 'login'	 =>	'sessions/new'
+	post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 	resources :rooms,  :replace_id_with => 'url'
 	resources :users
 
