d024479c28c758a2921fbde7128cc59a227cf809
Takagi Yuki <kiwi.kyuui@gmail.com>
2016-08-30 22:21:27 +0900

    微修正

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -4,8 +4,10 @@ class MessageBroadcastJob < ApplicationJob
   def perform(message)
     room_name = "room_#{Room.find(message.room_id).url}"
     u = User.find(message.user_id)
-      user = { id: u.id, name: u.name,
-               icon: u.icon}
+    gravatar_id = Digest::MD5::hexdigest(u.icon.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    icon = image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    user = { id: u.id, name: u.name, icon: icon}
     ActionCable.server.broadcast(room_name, { type: 'chat', data: { user: user ,message: message.message }})
   end
 end
