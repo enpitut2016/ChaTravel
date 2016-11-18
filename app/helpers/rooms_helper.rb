004e35011b2b3d9220cb382b8020cc5748efaab0
aoyamaS <aoyama@iplab.cs.tsukuba.ac.jp>
2016-11-19 03:14:04 +0900

    チャットメッセージのURLに対して<a>タグを挿入

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,17 @@
 module RoomsHelper
+
+require "uri"
+
+#urlにaタグを挿入する関数
+def text_url_to_link text
+
+  URI.extract(text, ['http']).uniq.each do |url|
+    sub_text = ""
+    sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+    text.gsub!(url, sub_text)
+  end
+  return text
+end
+
 end
