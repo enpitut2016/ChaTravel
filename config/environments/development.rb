960f85ad5b3052759b36a13e06acb725b5f990dd
SatoshiTanoue <again2222@gmail.com>
2016-11-23 16:15:15 +0900

    サーバーの設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,8 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.action_cable.allowed_request_origins = %w( http://192.168.33.12:3000 )
-  config.action_cable.allowed_request_origins += %w( http://localhost:3000 )
-
-end
+  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
+ end
